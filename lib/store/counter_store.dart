00db8e17736fe305c6d7565b15605b9ef903859d
joaopedroaquino <aqerhack@gmail.com>
2022-09-16 10:01:22 -0300

    generate .g file

--- lib/store/counter_store.dart
+++ lib/store/counter_store.dart
@@ -1,6 +1,23 @@
 import 'package:mobx/mobx.dart';
+
+//Arquivo .g gerado pelo build runner
 part 'counter_store.g.dart';
 
+//Classe usada pela base de codigo
 class CounterStore = _CounterStoreBase with _$CounterStore;
 
-abstract class _CounterStoreBase with Store {}
+//Classe da store
+abstract class _CounterStoreBase with Store {
+  //Observa a variavel, caso tenha a mudanca de estado
+  @observable
+  int value = 0;
+
+  //Acao usada para reconstruir o valor da variada qual o observable esta notificando
+  @action
+  void increment() {
+    value++;
+  }
+}
+
+
+//flutter pub run build_runner build para gerar o arquivo .g 
