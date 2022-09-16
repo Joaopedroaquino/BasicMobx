19518f6daa9d3dcf124db7d0814e08a5b9df7345
joaopedroaquino <aqerhack@gmail.com>
2022-09-16 10:14:11 -0300

    adicionando comentarios de documentacao da store

--- lib/store/counter_store.dart
+++ lib/store/counter_store.dart
@@ -4,20 +4,30 @@ import 'package:mobx/mobx.dart';
 part 'counter_store.g.dart';
 
 //Classe usada pela base de codigo
+// O código gerado estará dentro do arquivo part: counter.g.dart, que incluímos com a partdiretiva .
+//Sem isso, o build_runnernão produzirá qualquer saída.
+// O arquivo gerado contém o _$CounterStoremixin.
 class CounterStore = _CounterStoreBase with _$CounterStore;
 
 //Classe da store
+// A classe abstrata _CounterStore inclui o Storemixin.
+//Todo o seu código relacionado à store deve ser colocado dentro dessa classe abstrata.
+// Criamos uma classe Counter  para mesclar o código do build_runner.
+
 abstract class _CounterStoreBase with Store {
   //Observa a variavel, caso tenha a mudanca de estado
+  // anotação para marcar o value como observável.
   @observable
   int value = 0;
 
   //Acao usada para reconstruir o valor da variada qual o observable esta notificando
+  //action para marcar o increment()método como uma ação.
   @action
   void increment() {
     value++;
   }
 }
 
-
-//flutter pub run build_runner build para gerar o arquivo .g 
+//flutter pub run build_runner build para gerar o arquivo .g
+//em caso de conflitos com arquivos gerados usar flutter pub run build_runner build --delete-conflicting-outputs
+//flutter pub run build_runner watch gera o arquivo .g sempre que for salvado
